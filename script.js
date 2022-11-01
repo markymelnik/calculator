a57debb0010a653236e6ea05fd441ed9bce12b67
markymelnik <markemelnik@gmail.com>
2022-10-31 21:05:51 -0400

    Basic UI, modulo operator, decimal support

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 
 const numBtn = document.querySelectorAll(".numBtn");
 const signBtn = document.querySelectorAll(".signBtn");
-const clearBtn = document.querySelector("#clearBtn");
-const equalBtn = document.querySelector("#equalBtn");
-const displayValue = document.querySelector("#displayField");
+const decimalBtn = document.querySelector(".decimalBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const equalBtn = document.querySelector(".equalBtn");
+const displayValue = document.querySelector(".displayField");
 
 let previousOperand = 0;
 let currentOperand = 0;
@@ -23,6 +24,7 @@ numBtn.forEach(button => {
             currentOperand += button.textContent;
             currentOperand = +currentOperand;
             numResult = operate();
+            numResult = +numResult;
             operate();
         }
     })
@@ -30,26 +32,35 @@ numBtn.forEach(button => {
 
 signBtn.forEach(sign => {
     sign.addEventListener('click', () => {
-        if (numResult != null) {
-            displayValue.value += sign.textContent;
-            currentOperator = sign.textContent;
-            previousOperand = numResult;
-            currentOperand = 0;
-        } else {
+        if (numResult == null) {
             needSecondOperand = true;
             displayValue.value += sign.textContent;
             currentOperator = sign.textContent;
             previousOperand = currentOperand;
             currentOperand = 0;
+        } else {
+            displayValue.value += sign.textContent;
+            currentOperator = sign.textContent;
+            previousOperand = numResult;
+            currentOperand = 0;
         }
     })
 });
 
-equalBtn.addEventListener('click', () => {
-    displayValue.value = operate();
-    numResult = displayValue.value;
-    numResult = +numResult;
+decimalBtn.addEventListener('click', () => {
+    displayValue.value += decimalBtn.textContent;
+    currentOperand += decimalBtn.textContent;
 });
+
+equalBtn.addEventListener('click', () => {
+    numResult = operate();
+    let numLength = numResult.toString().length;
+    if (numLength > 8) {
+        let roundedValue = numResult.toFixed(3);
+        displayValue.value = roundedValue;
+    } else {
+    displayValue.value = numResult;
+}});
 
 clearBtn.addEventListener('click', () => {
     displayValue.value = null;
@@ -73,6 +84,8 @@ function operate(a, b, operator) {
             return multiply(a,b);
         case "/": 
             return divide(a,b);
+        case "%":
+            return modulo(a,b);
     }
 };
 
@@ -94,3 +107,7 @@ function divide(a, b) {
     } 
     return a / b;
 };
+
+function modulo(a, b) {
+    return a % b;
+}
